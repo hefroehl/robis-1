05169369729e1a572ce82290e1dad8b43cdaa5a8
Pieter <pieterprovoost@gmail.com>
2015-02-12 23:44:17 +0100

    color criterium

--- R/map_occurrences.R
+++ R/map_occurrences.R
@@ -1,8 +1,16 @@
-map_occurrences <- function(data) {
+map_occurrences <- function(data, color="tname") {
   world <- map_data(map="world")
   
-  blanktheme <- function() {
-    theme(axis.line=element_blank(),
+  p <- ggplot(world, aes(long, lat)) +
+    geom_polygon(aes(group=group), fill="gray90", color="gray90", size=0.2) +
+    geom_point(data=data, aes_string("longitude", "latitude", color=color), alpha=0.4, size=3) +
+    labs(x="", y="") +
+    theme_bw(base_size=14) +
+    theme(
+      legend.position="bottom", 
+      legend.key=element_blank(), 
+      legend.title=element_blank(),
+      axis.line=element_blank(),
       axis.text.x=element_blank(),
       axis.text.y=element_blank(),
       axis.ticks=element_blank(),
@@ -12,22 +20,10 @@ map_occurrences <- function(data) {
       panel.border=element_blank(),
       panel.grid.major=element_blank(),
       panel.grid.minor=element_blank(),
-      plot.background=element_blank())
-  }
-  
-  p <- ggplot(world, aes(long, lat)) +
-    geom_polygon(aes(group=group), fill="gray90", color="gray90", size=0.2) +
-    geom_point(data=data, aes(longitude, latitude, color=tname), alpha=0.4, size=3) +
-    labs(x="", y="") +
-    theme_bw(base_size=14) +
-    theme(
-      legend.position="bottom", 
-      legend.key=element_blank(), 
-      legend.title=element_blank()
+      plot.background=element_blank()
     ) +
     guides(col = guide_legend(nrow=2)) +
-    coord_fixed(ratio=1) +
-    blanktheme()
+    coord_fixed(ratio=1)
   
   print(p)
 
