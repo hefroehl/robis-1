ceb737e440156ab1b2d086d195ed57696b7e2103
Pieter <pieter@Pieters-MacBook-Pro.local>
2015-02-12 09:33:37 +0100

    null check fix

--- R/occurrences.R
+++ R/occurrences.R
@@ -1,7 +1,7 @@
 get_occurrences <- function(species, maxFeatures=NULL) {
   result <- NULL
   url <- paste0(obis_url(service="WFS"), "&request=GetFeature&typeName=OBIS:points_ex&outputFormat=csv")
-  if (maxFeatures > 0) {
+  if (!is.null(maxFeatures)) {
     url <- paste0(url, "&maxFeatures=", maxFeatures)
   }
   for (name in species) {
