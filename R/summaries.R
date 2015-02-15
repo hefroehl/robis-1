5fbc9de1837c52dc483160ebc6873913dcc0cb97
Pieter <pieterprovoost@gmail.com>
2015-02-15 01:33:46 +0100

    hexgrid

--- R/summaries.R
+++ R/summaries.R
@@ -16,3 +16,20 @@ get_summaries <- function(maxFeatures=NULL, table="map5deg_with_geom") {
   
   return(read.csv(text=csv))
 }
+
+get_hexsummaries <- function(typeName="hexgrid4", maxFeatures=NULL) {
+  url <- obis_url(service="WFS")
+  parms <- c(
+    service="WFS",
+    version="1.0.0",
+    request="GetFeature",
+    typeName=typeName,
+    outputFormat="csv",
+    maxFeatures=maxFeatures
+  )
+  
+  response <- GET(url, query=compact(as.list(parms)))
+  csv <- content(response, as = "text")
+  
+  return(read.csv(text=csv))
+}
