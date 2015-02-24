fc3d84e4f624f7741458885a1fa46ae9e7ba1de8
unknown <pieterprovoost@gmail.com>
2015-02-24 14:04:46 +0100

    table name check

--- R/distribution.R
+++ R/distribution.R
@@ -3,12 +3,16 @@
 #' Retrieves distribution data
 #' 
 #' @param identifier species name
-#' @param table table
+#' @param table table, one of \code{dist_sp_5deg}, \code{dist_sp_1deg}, \code{dist_sp_05deg}, \code{dist_sp_01deg}
 #' @param maxFeatures maximum number of features
 #' @param ... additional arguments for \code{\link{wfs_request}}
 #' 
 #' @return distribution data
 get_distribution <- function(identifier, table="dist_sp_5deg", maxFeatures=NULL, ...) {
+  
+  if (! table %in% c("dist_sp_5deg", "dist_sp_1deg", "dist_sp_05deg", "dist_sp_01deg")) {
+    stop("Table must be one of dist_sp_5deg, dist_sp_1deg, dist_sp_05deg, dist_sp_01deg")
+  }
   
   viewparams <- NULL
   viewparams[["where"]] <- paste0("scientific='", identifier, "'")
