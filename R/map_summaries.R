eae70661d1ce96b7fdd2f5ccf7bf8f66f3f90f75
unknown <infppro@NB-PVM-IT-9232.admbnet.be>
2015-04-13 14:28:58 +0200

    customize summary breaks and colors

--- R/map_summaries.R
+++ R/map_summaries.R
@@ -4,22 +4,33 @@
 #' 
 #' @param data data frame from \code{\link{get_summaries}} or \code{\link{get_hexsummaries}}
 #' @param summary summary name to be used, either \code{simpson} or \code{shannon}
-map_summaries <- function(data, summary="shannon") {
+#' @param breaks breaks vector
+#' @param colors colors vector
+#' @param labels labels vector
+map_summaries <- function(data, summary="shannon", breaks=NULL, colors=NULL, labels=NULL) {
 
-  names <- c("simpson", "shannon")
+  names <- c("simpson", "shannon", "s", "es")
   
   if (! summary %in% names) {
     stop(paste0("Summary must be one of ", paste(names, collapse=", ")))
   }
   
   if (summary == "simpson") {
-    breaks <- c(0, 0.11, 0.23, 0.35, 0.48, 0.61, 0.76, 1.00)
-    labels <- c("0.00-0.11", ">0.11-0.23", ">0.23-0.35", ">0.35-0.48", ">0.48-0.61", ">0.61-0.76", ">0.76-1.00")
-    colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
+    if (is.null(breaks)) breaks <- c(0, 0.11, 0.23, 0.35, 0.48, 0.61, 0.76, 1.00)
+    if (is.null(labels)) labels <- c("0.00-0.11", ">0.11-0.23", ">0.23-0.35", ">0.35-0.48", ">0.48-0.61", ">0.61-0.76", ">0.76-1.00")
+    if (is.null(colors)) colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
   } else if (summary == "shannon") {
-    breaks <- c(0, 1, 2.1, 3.2, 4.3, 5.4, 6.5, 8.3)
-    labels <- c("0.0-1.0", ">1.0-2.1", ">2.1-3.2", ">3.2-4.3", ">4.3-5.4", ">5.4-6.5", ">6.5-8.3")
-    colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
+    if (is.null(breaks)) breaks <- c(0, 1, 2.1, 3.2, 4.3, 5.4, 6.5, 8.3)
+    if (is.null(labels)) labels <- c("0.0-1.0", ">1.0-2.1", ">2.1-3.2", ">3.2-4.3", ">4.3-5.4", ">5.4-6.5", ">6.5-8.3")
+    if (is.null(colors)) colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
+  } else if (summary == "s") {
+    if (is.null(breaks)) breaks <- c(0, 1000, 2000, 4000, 6000, 8000, 10000, 12000)
+    if (is.null(labels)) labels <- c("0-1000", ">1000-2000", ">2000-4000", ">4000-6000", ">6000-8000", ">8000-10000", ">10000-12000")
+    if (is.null(colors)) colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
+  } else if (summary == "es") {
+    if (is.null(breaks)) breaks <- c(0, 27, 38, 42, 44, 46, 48, 50)
+    if (is.null(labels)) labels <- c("0-27", ">27-38", ">38-42", ">42-44", ">44-46", ">46-48", ">48-50")
+    if (is.null(colors)) colors <- c("blue", "purple", "#56B4E9", "green", "yellow", "orange", "red")
   }
   
   world <- map_data(map="world")
